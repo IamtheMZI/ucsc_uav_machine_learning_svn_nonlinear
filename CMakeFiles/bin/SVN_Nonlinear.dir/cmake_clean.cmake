37f535e005d01df0974656fb53c3c054982afeda
UCSC UAV <muhammadzilahee@gmail.com>
2016-02-06 00:36:16 -0800

    Version 1

---
+++ CMakeFiles/bin/SVN_Nonlinear.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bin/SVN_Nonlinear.dir/src/SVN_Nonlinear_rng.cpp.o"
+  "bin/SVN_Nonlinear.pdb"
+  "bin/SVN_Nonlinear"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bin/SVN_Nonlinear.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
